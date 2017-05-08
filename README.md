e0ad4de05530a7c31d450763b1a268ebeb6e2ba4
Divya Mamgai <divyamamgai21@gmail.com>
2017-05-08 15:21:50 +0530

    Initial Version

---
+++ README.md
@@ -0,0 +1,12 @@
+jQueryBoilerPlate
+======
+A simple boiler plate code for jQuery plugin development with NPM.
+
+1. Implements build procedure using NPM commands.
+
+2. Uses Stylus CSS preprocessor.
+
+3. To build simply run
+```shell
+npm build
+```
---
+++ public/resources/js/main.js
@@ -0,0 +1,86 @@
+(function ($, w, d) {
+    'use strict';
+    /** @type JQuery */
+    var $clickCount,
+        $imageContainer,
+        $image,
+        $clickSounds,
+        $restartButton,
+        $loadingPercentage;
+    var clickCount = 0;
+
+    function xhrOnError() {
+        alert('An error occurred while fetching a cure Cat image!\nRetrying in 5 seconds...');
+    }
+
+    function xhrOnProgress(event) {
+        if (event.lengthComputable) {
+            $loadingPercentage.text(Math.round(event.loaded / event.total * 100) + '%');
+        }
+    }
+
+    function initialize() {
+        if (!$restartButton.hasClass('loading')) {
+            $restartButton.addClass('loading');
+            $loadingPercentage.addClass('show');
+            var xhr = new XMLHttpRequest();
+            xhr.onprogress = xhrOnProgress;
+            xhr.onerror = xhrOnError;
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                    var url = window.URL || window.webkitURL;
+                    $image.css('background-image', 'url(' + url.createObjectURL(xhr.response) + ')');
+                    $clickCount.text(clickCount = 0);
+                    $restartButton.removeClass('loading');
+                    $loadingPercentage
+                        .removeClass('show')
+                        .text('0%');
+                }
+            };
+            xhr.open('GET', 'http://thecatapi.com/api/images/get?api_key=MTgyMTA2&format=src&category=dream');
+            xhr.responseType = 'blob';
+            xhr.send();
+        }
+    }
+
+    function clickSoundPlay(clickSound) {
+        clickSound.currentTime = 0;
+        clickSound.play();
+    }
+
+    function imageContainerOnMouseDown() {
+        $imageContainer.addClass('clicked');
+        $clickCount.addClass('image-clicked');
+        $clickCount.text(++clickCount);
+        clickSoundPlay($clickSounds.get(Math.floor(Math.random() * $clickSounds.length)));
+    }
+
+    function imageContainerOnMouseUp() {
+        $imageContainer.removeClass('clicked');
+        $clickCount.removeClass('image-clicked');
+    }
+
+    function restartButtonOnClick(event) {
+        event.stopPropagation();
+        initialize();
+    }
+
+    $(function () {
+        $clickSounds = $('.click-sound', d);
+        $clickSounds.each(function (index) {
+            $clickSounds.get(index).volume = 0.2;
+        });
+        $imageContainer = $('#image-container', d)
+            .on('mousedown', imageContainerOnMouseDown)
+            .on('mouseup', imageContainerOnMouseUp);
+        $image = $('#image', $imageContainer);
+        $clickCount = $('#click-count', $imageContainer);
+        $restartButton = $('#restart-button', $imageContainer)
+            .on('click', restartButtonOnClick)
+            .on('mousedown mouseup', function (event) {
+                event.stopPropagation();
+            });
+        $loadingPercentage = $('#loading-percentage', $imageContainer);
+        initialize();
+    });
+})(jQuery, window, document);
